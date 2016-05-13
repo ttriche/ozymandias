ef4e20f4af20b43fabb5a7ec312496f76fefe3b9
ttriche <tim.triche@gmail.com>
2016-05-13 16:11:33 -0700

    automate mapping of 27k, 450k, and EPIC SNP probes

---
+++ R/preprocessNoob27k.R
@@ -0,0 +1,73 @@
+#' avoid consulting an annotation package for a platform with only Type I probes
+#' 
+#' @param rgSet   an RGChannelSet
+#' @param offset  offset term for intensities (default is 15)
+#' @param dyeCorr ignored (no need on 27k)
+#' @param verbose make noise? (TRUE) 
+#' 
+#' @return        a MethylSet
+#'
+#' @export 
+preprocessNoob <- function(rgSet, offset=15, dyeCorr=FALSE, verbose = TRUE) { 
+
+  .isRG(rgSet)
+  manifest <- getManifest(rgSet)
+  subverbose <- max(as.integer(verbose) - 1L, 0)
+
+  ## Extraction of the out-of-band controls
+  controls <- getOOB(rgSet) # works fine on 27k
+  names(controls) <- c("Cy3", "Cy5")
+  mset <- preprocessRaw(rgSet)
+  meth <- getMeth(mset)
+  unmeth <- getUnmeth(mset)
+
+  if (any(meth<=0)) meth[which(meth<=0)] <- 1
+  if (any(unmeth<=0)) unmeth[which(unmeth<=0)] <- 1
+
+  G.probes <- getProbeInfo(manifest, type="I-Green")$Name
+  cy3.probes <- which(rownames(mset) %in% G.probes)
+  R.probes <- getProbeInfo(manifest, type="I-Red")$Name
+  cy5.probes <- which(rownames(mset) %in% R.probes)
+
+  dat <- list(Cy3 = list(M =  as.matrix(meth[cy3.probes,]), 
+                         U =  as.matrix(unmeth[cy3.probes,])),
+              Cy5 = list(M =  as.matrix(meth[cy5.probes,]), 
+                         U =  as.matrix(unmeth[cy5.probes,])))
+
+  rows <- lapply(dat, function(ch) {
+    sapply(names(ch), function(nch) {
+      nrow(ch[[nch]])
+    })
+  })
+  last <- lapply(rows, cumsum)
+  first <- lapply(names(last), function(nch) {
+    last[[nch]] - rows[[nch]] + 1
+  })
+  names(first) <- names(last)
+
+  estimates <- lapply(names(dat), function(nch) { 
+    xf <- rbind(dat[[nch]][['M']], dat[[nch]][['U']])
+    xs <- minfi:::normexp.get.xs(xf = xf, controls = controls[[nch]], 
+                                 offset=offset, verbose = subverbose)
+    names(xs[['params']]) <- paste(names(xs[['params']]), nch, sep='.')
+    names(xs[['meta']]) <- paste(names(xs[['meta']]), nch, sep='.')
+    xs
+  })
+  names(estimates) <- names(dat)
+
+  cy3.M <- first[['Cy3']][['M']]:last[['Cy3']][['M']]
+  meth[cy3.probes, ] <- estimates[['Cy3']][['xs']][cy3.M,]
+  cy3.U <- first[['Cy3']][['U']]:last[['Cy3']][['U']]
+  unmeth[cy3.probes,] <- estimates[['Cy3']][['xs']][cy3.U,]
+
+  cy5.M <- first[['Cy5']][['M']]:last[['Cy5']][['M']]
+  meth[cy5.probes,] <- estimates[['Cy5']][['xs']][cy5.M,]
+  cy5.U <- first[['Cy5']][['U']]:last[['Cy5']][['U']]
+  unmeth[cy5.probes,] <- estimates[['Cy5']][['xs']][cy5.U,]
+
+  assayDataElement(mset, "Meth") <- meth
+  assayDataElement(mset, "Unmeth") <- unmeth
+  mset@preprocessMethod <- c(preprocessMethod(mset), 
+                             mu.norm = sprintf("Noob, dyeCorr=%s", dyeCorr))
+  return(mset)
+}
---
+++ R/rgSetToGrSet.R
@@ -0,0 +1,42 @@
+#' Take the result of IDATsToRgSet and preprocess it (pvals, SNPs, etc.)
+#' 
+#' @param rgSet   an RGChannelSet 
+#' @param pcutoff p-value cutoff to set a beta value to NA (0.01)
+#' @param genome  genome build to annotate against if not using funnorm (hg19)
+#' @param funnorm run preprocessFunnnorm() to get the grSet? (FALSE, use noob)
+#' @param gr      an optional GenomicRanges to annotate the rows of the result
+#'
+#' @return  a GenomicRatioSet with metadata(grSet)$SNPs and p > 0.01 set to NA
+#' 
+#' @export
+rgSetToGrSet <- function(rgSet, pcutoff=0.01, genome=c("hg19","hg38"),
+                         funnorm=FALSE, gr=NULL) {
+
+  if (funnorm) {
+    grSet <- preprocessFunnorm(rgSet)
+  } else { 
+    # needs monkeypatching
+    mSet <- ifelse(annotation(rgSet)["array"] == "IlluminaHumanMethylation27k",
+                   preprocessNoob27k(rgSet),
+                   preprocessNoob(rgSet))
+    genome <- match.arg(genome)
+    if (is.null(gr)) {
+      library(paste0("FDb.InfiniumMethylation.", genome), character.only=TRUE)
+      gr <- switch(annotation(rgSet)["array"],
+                   "IlluminaHumanMethylation27k"=get27k(),
+                   "IlluminaHumanMethylation450k"=get450k(),
+                   "IlluminaHumanMethylationEPIC"=getEPIC())
+    }
+    grSet <- GenomicRatioSet(gr=gr[rownames(mset)], Beta=getBeta(mset), 
+                             preprocessMethod=preprocessMethod(mset), 
+                             CN=getCN(mset), pData=pData(mset),
+                             annotation=annotation(mset))
+  }
+
+  metadata(grSet)$SNPs <- getSnpBetas(rgSet)
+  assays(grSet)$pval <- matrix(NA_real_, ncol=ncol(grSet), nrow=nrow(grSet))
+  assays(grSet)$pval <- detectionP(rgSet)[rownames(grSet), colnames(grSet)]
+  is.na(assays(grSet)$Beta[which(assays(grSet)$pval > pcutoff)]) <- TRUE 
+  return(grSet)
+
+}
