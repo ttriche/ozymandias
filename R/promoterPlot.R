f19543a8b21a544a117f0c5ba90a6e87bb778df8
ttriche <tim.triche@gmail.com>
2016-05-31 16:54:36 -0700

    make promoterPlot work for multi-panel promoter plots

--- R/promoterPlot.R
+++ R/promoterPlot.R
@@ -3,17 +3,25 @@
 #' 
 #' @param grSet     the GenomicRatioSet from which to get the data
 #' @param promoter  the promoter or other range to overlap 
+#' @param rotate    rotate the plot so that samples are rows? (TRUE) 
+#' @param flank     flank the given region(s) upstream? (TRUE) 
 #' @param ...       other arguments passed on to Heatmap()
 #' 
 #' @import ComplexHeatmap
 #' @import impute 
 #' 
 #' @export
-promoterPlot <- function(grSet, promoter, ...) { 
+promoterPlot <- function(grSet, promoter, rotate=TRUE, flank=TRUE, ...) { 
+  if (flank == TRUE) promoter <- promoters(promoter)
   res <- sort(unique(subsetByOverlaps(grSet, promoter)))
   rownames(res) <- as(granges(res), "character") 
   betas <- getBeta(res)
   if (any(is.na(betas))) betas <- impute.knn(betas)$data
-  Heatmap(t(betas), cluster_columns=FALSE, col=jet, name="Methylation", 
-          row_names_gp=gpar(fontsize=7), column_names_gp=gpar(fontsize=7), ...)
+  if (rotate == TRUE) {
+    Heatmap(betas, cluster_rows=FALSE, col=jet, name="Methylation", 
+      row_names_gp=gpar(fontsize=7), column_names_gp=gpar(fontsize=7), ...)
+  } else { 
+    Heatmap(t(betas), cluster_columns=FALSE, col=jet, name="Methylation", 
+      row_names_gp=gpar(fontsize=7), column_names_gp=gpar(fontsize=7), ...)
+  }
 } 
