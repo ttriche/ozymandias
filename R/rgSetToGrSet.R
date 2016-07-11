9e18870708f458414bd76baa38d00fc11dab9e91
ttriche <tim.triche@gmail.com>
2016-07-11 06:37:32 -0700

    fix rgSetToGrSet and default to single-sample noob

--- R/rgSetToGrSet.R
+++ R/rgSetToGrSet.R
@@ -1,48 +1,44 @@
 #' Take the result of IDATsToRgSet and preprocess it (pvals, SNPs, etc.)
 #' 
-#' @param rgSet   an RGChannelSet 
-#' @param pcutoff p-value cutoff to set a beta value to NA (0.01)
-#' @param genome  genome build to annotate against if not using funnorm (hg19)
-#' @param funnorm run preprocessFunnnorm() to get the grSet? (FALSE, use noob)
-#' @param gr      an optional GenomicRanges to annotate the rows of the result
-#' @param ...     additional arguments for preprocessNoob (eg. how="reference")
+#' @param rgSet     an RGChannelSet 
+#' @param pcutoff   p-value cutoff to set a beta value to NA (0.01)
+#' @param funnorm   run preprocessFunnnorm() to get the grSet? (FALSE, use noob)
+#' @param dyeMethod How to fix dye bias: reference or single sample approach?
+#' @param genome    genome build to annotate against if not using funnorm (hg19)
+#' @param gr        an optional GenomicRanges to annotate the rows of the result
 #'
 #' @return  a GenomicRatioSet with metadata(grSet)$SNPs and p > 0.01 set to NA
 #' 
 #' @export
-rgSetToGrSet <- function(rgSet, pcutoff=0.01, genome=c("hg19","hg38","hg18"),
-                         funnorm=FALSE, gr=NULL, ...) {
+rgSetToGrSet <- function(rgSet, pcutoff=0.01, funnorm=FALSE, dyeMethod="single",
+                         genome=c("hg19","hg38","hg18"), gr=NULL, ...) {
 
-  if (funnorm) {
-    grSet <- preprocessFunnorm(rgSet)
-  } else { 
-    # needs monkeypatching
-    if (annotation(rgSet)["array"] == "IlluminaHumanMethylation27k") {
-      mset <- preprocessNoob27k(rgSet, ...)
-    } else {
-      mset <- preprocessNoob(rgSet, ...)
-    }
-    
-    genome <- match.arg(genome)
-    if (is.null(gr)) {
-      library(paste0("FDb.InfiniumMethylation.", genome), character.only=TRUE)
-      gr <- switch(annotation(rgSet)["array"],
-                   "IlluminaHumanMethylation27k"=get27k(),
-                   "IlluminaHumanMethylation450k"=get450k(),
-                   "IlluminaHumanMethylationEPIC"=getEPIC())
-    }
-    # drop probes that don't map to the target genome, e.g. hg19 
+  platform <- annotation(rgSet)["array"]
+  if (genome != "hg19") stop("Genomes besides hg19 are currently unsupported.")
+  process <- ifelse(funnorm, "funnorm", 
+                    ifelse(platform == "IlluminaHumanMethylation27k", "noob27k",
+                           "noob")) # default to noob
+
+  map27k <- function(rgSet) { # {{{
+    library(paste0("FDb.InfiniumMethylation.", genome), character.only=TRUE)
+    mset <- preprocessNoob27k(rgSet, ...)
+    gr <- get27k()
     mset <- mset[which(rownames(mset) %in% names(gr)), ] # only alignable
-    grSet <- GenomicRatioSet(gr=gr[rownames(mset)], Beta=getBeta(mset), 
-                             preprocessMethod=preprocessMethod(mset), 
-                             CN=getCN(mset), pData=pData(mset),
-                             annotation=annotation(mset))
-  }
+    GenomicRatioSet(gr=gr[rownames(mset)], Beta=getBeta(mset), 
+                    preprocessMethod=preprocessMethod(mset),
+                    CN=getCN(mset), pData=pData(mset),
+                    annotation=annotation(mset))
+  } # }}}
 
+  dm <- dyeMethod
+  grSet <- switch(process,
+                  funnorm=preprocessFunnorm(rgSet, ...),
+                  noob27k=map27k(preprocessNoob27k(rgSet, ...)),
+                  noob=ratioConvert(mapToGenome(preprocessNoob(rgSet, 
+                                                               dyeMethod=dm))))
   metadata(grSet)$SNPs <- getSnpBeta(rgSet)
   assays(grSet)$pval <- matrix(NA_real_, ncol=ncol(grSet), nrow=nrow(grSet))
   assays(grSet)$pval <- detectionP(rgSet)[rownames(grSet), colnames(grSet)]
   is.na(assays(grSet)$Beta[which(assays(grSet)$pval > pcutoff)]) <- TRUE 
   return(grSet)
-
 }
